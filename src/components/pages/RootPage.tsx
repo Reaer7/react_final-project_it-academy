1664da6ec67be9d60a2c4289cd39bf74a39522e1
Brutski <last.reater@gmail.com>
2024-02-14 02:10:10 +0300

    wip: adding carousel

--- src/components/pages/RootPage.tsx
+++ src/components/pages/RootPage.tsx
@@ -18,23 +18,19 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 const images = [
     {
         label: 'San Francisco – Oakland Bay Bridge, United States',
-        imgPath:
-            'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+        imgPath: 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
     },
     {
         label: 'Bird',
-        imgPath:
-            'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+        imgPath: 'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
     },
     {
         label: 'Bali, Indonesia',
-        imgPath:
-            'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+        imgPath: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
     },
     {
         label: 'Goč, Serbia',
-        imgPath:
-            'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+        imgPath: 'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
     },
 ];
 
@@ -67,7 +63,7 @@ export function RootPage() {
                 id="page.root.head"
             />
         </h4>
-        <Box sx={{ maxWidth: 600, flexGrow: 1 }}>
+        <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
             <Paper
                 square
                 elevation={0}
